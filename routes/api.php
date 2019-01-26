91c758502bb08e57183bf0fe5d257f65a490f02c
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-01-26 12:03:54 +0800

    Add routes for http requests

--- routes/api.php
+++ routes/api.php
@@ -51,8 +51,10 @@ Route::post('authenticate', 'Auth\AuthController@authenticate');
 
     //user patient 
     Route::get('users/profilePatients', 'Users\ProfilePatientController@index');
-
-
+    Route::get('users/profilePatient/{user_id}', 'Users\ProfilePatientController@show');
+    Route::get('users/patients/{doctor_id}', 'Users\ProfilePatientController@patients');
+    Route::get('users/patientsCount/{doctor_id}', 'Users\ProfilePatientController@patientsCount');
+    Route::resource('users', 'Users\ProfilePatientController');
 
 //Route::group(['middleware' => 'jwt.auth'], function () {
     /* User */
@@ -76,8 +78,16 @@ Route::post('authenticate', 'Auth\AuthController@authenticate');
     Route::post('cms/survey/create', 'ContinuousCare\FeedbackController@create');
     Route::post('cms/survey/update/{id}', 'ContinuousCare\FeedbackController@update');
     Route::post('cms/survey/delete/{id}', 'ContinuousCare\FeedbackController@delete');
-
+    
     /*Bookings*/
-    Route::get('bookings', 'Booking\BookingController@index');
+    Route::get('bookings/{user_id}', 'Booking\BookingController@show');
+    Route::get('bookings/showAllByPatient/{patient_id}', 'Booking\BookingController@showAllByPatient');
+    Route::get('bookings/showAllByDoctor/{doctor_id}', 'Booking\BookingController@showAllByDoctor');
+    Route::get('bookings/patientRecentBooking/{patient_id}', 'Booking\BookingController@patientRecentBooking');
+    Route::get('bookings/doctorRecentBooking/{doctor_id}', 'Booking\BookingController@doctorRecentBooking');
+    Route::get('bookings/bookingsCount/{doctor_id}', 'Booking\BookingController@bookingsCount');
+
+    /*Prescription*/
+    Route::get('prescription/{patient_id}', 'Prescription\PrescriptionController@show');
 
 //});
