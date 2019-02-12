8c4a10ec29e1e218b044f2d1c2c88e6b3b2dc30b
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-02-12 23:58:35 +0800

    Add chart components

---
+++ resources/js/components/common/BarChart.vue
@@ -0,0 +1,70 @@
+<script>
+import { Line, HorizontalBar } from 'vue-chartjs'
+
+export default {
+    name: 'BarChart',
+    extends: HorizontalBar,
+    mounted () {
+        // Overwriting base render method with actual data.
+        this.renderChart({
+        labels: ["January", "February", "March", "April", "May"],
+        datasets: [{
+            label: "My First dataset",
+            data: [65, 59, 80, 81, 56],
+            backgroundColor: "#00BFA5",
+            hoverBackgroundColor: "rgba(0,191,165,.8)",
+            borderColor: "transparent"
+        }, {
+            label: "My Second dataset",
+            data: [28, 48, 40, 19, 86],
+            backgroundColor: "#1DE9B6",
+            hoverBackgroundColor: "rgba(29,233,182,.8)",
+            borderColor: "transparent"
+        },
+        {
+            label: "My Third dataset",
+            data: [80, 25, 16, 36, 67],
+            backgroundColor: "#64FFDA",
+            hoverBackgroundColor: "rgba(100,255,218,.8)",
+            borderColor: "transparent"
+        }]
+    
+        },{
+            title:{
+            display:false,
+            text:"Chart.js Bar Chart - Stacked"
+        },
+        tooltips: {
+            mode: 'label'
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        responsiveAnimationDuration:500,
+        scales: {
+            xAxes: [{
+                stacked: true,
+                display: true,
+                gridLines: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                scaleLabel: {
+                    display: true,
+                }
+            }],
+            yAxes: [{
+                stacked: true,
+                display: true,
+                gridLines: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                scaleLabel: {
+                    display: true,
+                }
+            }]
+        }
+        })
+    }
+}
+</script>
---
+++ resources/js/components/common/Chart.vue
@@ -0,0 +1,65 @@
+<script>
+import { Line, HorizontalBar } from 'vue-chartjs'
+
+export default {
+    name: 'Chart',
+    extends: Line,
+    mounted () {
+        // Overwriting base render method with actual data.
+        this.renderChart({
+       labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [{
+            label: "Sales",
+            data: [32, 25, 45, 30, 60, 40, 72, 52, 80, 60, 92, 70],
+            lineTension: 0,
+            fill: false,
+            // borderDash: [5, 5],
+            borderColor: "#37BC9B",
+            pointBorderColor: "#37BC9B",
+            pointBackgroundColor: "#FFF",
+            pointBorderWidth: 3,
+            pointRadius: 6,
+        }]
+        },{
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                position: 'top',
+                labels: {
+                    boxWidth: 10,
+                    fontSize: 14
+                },
+            },
+            hover: {
+                mode: 'label'
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        lineWidth: 2,
+                        color: "rgba(0, 0, 0, 0.05)",
+                        zeroLineWidth: 2,
+                        zeroLineColor: "rgba(0, 0, 0, 0.05)",
+                        drawTicks: false,
+                    },
+                    ticks: {
+                        fontSize: 14,
+                    }
+                }],
+                yAxes: [{
+                    display: false,
+                    ticks: {
+                        min: 0,
+                        max: 100
+                    }
+                }]
+            },
+            title: {
+                display: false,
+                text: 'Chart.js Line Chart - Legend'
+            }
+        })
+    }
+}
+</script>
