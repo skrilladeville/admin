dbbafd80873c4e8ae947fd1833b901e40a29985a
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-02-12 23:49:11 +0800

    Add font-awesome icon components

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,16 +7,22 @@ import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css';
 import VueGoodTablePlugin from 'vue-good-table';
+import VueCharts from 'vue-chartjs'
+import { Bar, Line } from 'vue-chartjs'
+
 // import the styles
 import 'vue-good-table/dist/vue-good-table.css'
 
 /*Font-Awesome Icons*/
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee, faCalendarCheck, faUser, 
-	faStore, faClock, faFileInvoice, faThumbsUp, faTimes } from '@fortawesome/free-solid-svg-icons'
+	faStore, faClock, faFileInvoice, faThumbsUp, faTimes,
+	faShoppingCart, faMoneyBillWave, faTags, faFileInvoiceDollar, 
+	faLandmark, faPercent } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCoffee, faCalendarCheck, faUser, faStore, faClock, faFileInvoice, faThumbsUp, faTimes)
+library.add(faCoffee, faCalendarCheck, faUser, faStore, faClock, faFileInvoice, faThumbsUp, faTimes,
+	faShoppingCart, faMoneyBillWave,faTags, faFileInvoiceDollar, faLandmark, faPercent)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueGoodTablePlugin);
 
