74d3d658fe0af8a68f776b101aa5beb00611fded
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-03-06 01:50:12 +0800

    Add icons and moment module

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,9 @@ import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css';
 //import vue-tables-2
 import {ServerTable, ClientTable, Event} from 'vue-tables-2';
-Vue.use(ClientTable, {}, false, 'bootstrap4');
+Vue.use(ClientTable, {}, true, 'bootstrap4');
 Vue.use(ServerTable, {}, false, 'bootstrap4');
-
+Vue.use(Event)
 
 import VueGoodTablePlugin from 'vue-good-table';
 import VueCharts from 'vue-chartjs'
@@ -25,17 +25,18 @@ import { faCoffee, faCalendarCheck, faUser,
 	faStore, faClock, faFileInvoice, faThumbsUp, faTimes,
 	faShoppingCart, faMoneyBillWave, faTags, faFileInvoiceDollar, 
 	faLandmark, faPercent, faEye, faEllipsisH, faTimesCircle,
-	faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+	faCheckCircle, faBan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faCoffee, faCalendarCheck, faUser, faStore, faClock, faFileInvoice, faThumbsUp, faTimes,
 	faShoppingCart, faMoneyBillWave,faTags, faFileInvoiceDollar, faLandmark, faPercent,faEye, faEllipsisH,
-	faTimesCircle, faCheckCircle)
+	faTimesCircle, faCheckCircle, faBan)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueGoodTablePlugin);
 
 window.$ = window.jQuery = $;
 window.axios = axios;
+window.moment = require('moment');
 require('bootstrap');
 
 Vue.use( VueNoty, {
